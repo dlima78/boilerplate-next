0e0270d63bcd22a248b48426d7ef86d3aef79164
Edu Lima <dlima78@hotmai.com>
2024-04-10 11:45:16 -0300

    Jest styled-components config

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,14 @@ const config: Config = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/app/**',
+    '!src/lib/registry.tsx',
+    '!src/styles/**',
+    '!src/types/**'
+  ],
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageProvider: 'v8',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
@@ -13,6 +20,13 @@ const config: Config = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  // https://github.com/styled-components/styled-components/issues/4081
+  // v6 of styled-components doesn't inject styles in test environment
+  // we should to force it to use the browser version
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
 
